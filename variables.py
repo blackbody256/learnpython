90b7c13f789da07060d6306166111474bb291f92
blackbody256 <sseluyindaeva@gmail.com>
2023-10-25 13:24:46 +0300

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,23 @@
+#variable is a allocated memory space that stores values
+first_name= "black"#A string is a group of charaters
+second_name= "Body"
+full_name= first_name+" "+second_name
+print("Hello "+full_name)
+print(type(full_name))
+#a integer data type is use to store integers
+age = 19
+#age =age+1
+age+=1
+print("Your age is "+ str(age))
+print(type(age))
+#float data type stores decimal numbers
+height= 171.45
+print("your height is "+str(height)+"cm")
+print(type(height))
+#boalean is a varible that can only store true or false
+human = False
+print(human)
+print(type(human))
+print("Are a human "+str(human))
+#booleans are used most in if statements
+
