eeacf14ad983785300cabbe5fb73a9919b2c2c7c
blackbody256 <sseluyindaeva@gmail.com>
2023-10-25 13:24:12 +0300

    Create multiple assignment.py

---
+++ multiple assignment.py
@@ -0,0 +1,18 @@
+#multiple assignment  allows us to assign multipe variables at the same times in one of code
+#name ="Black body"
+#age = 19
+#attractive = True
+name,  age, attractive = "Black Body", 19, True
+print(name)
+print(age)
+print(attractive)
+
+#spongebob = 30
+#patrick = 30
+#sandy = 30
+#squidward = 30
+spongebob=patrick=sandy=squidward=30
+print(spongebob)
+print(sandy)
+print(patrick)
+print(squidward)
